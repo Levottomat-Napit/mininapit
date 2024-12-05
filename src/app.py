1d8720205512dccb26a2a87092bc479e6dd2c27e
Kristian Cerf <kraastian2@gmail.com>
2024-12-05 13:14:20 +0200

    Added sorting functionality

--- src/app.py
+++ src/app.py
@@ -14,8 +14,10 @@ from entities.citation import (
 
 @app.get('/')
 def index():
-    citations = get_citations()
+    sort_by = request.args.get('sort_by', None)
 
+    citations = get_citations(sort_by=sort_by)
+    
     return render_template('index.html', citations=citations)
 
 @app.get('/new')
@@ -55,6 +57,11 @@ def inproceedings_new():
     create_inproceedings(info)
 
     return redirect('/')
+
+@app.route('/sort')
+def sort():
+    
+    return render_template('/')
 
 @app.post('/delete')
 def delete_citation():
--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -2,11 +2,23 @@ from sqlalchemy import text
 from config import db
 from entities.citation import Article, Inproceedings
 
-def get_citations():
+def get_citations(sort_by=None):
     articles = db.session.execute(text('SELECT * FROM articles')).fetchall()
     inproceedings = db.session.execute(text('SELECT * FROM inproceedings')).fetchall()
 
-    return [Article(*art) for art in articles] + [Inproceedings(*ip) for ip in inproceedings]
+
+    citations = [Article(*art) for art in articles] + [Inproceedings(*ip) for ip in inproceedings]
+
+    if sort_by == None:
+        pass
+    elif sort_by == 'year':
+        citations.sort(key=lambda x: x.year, reverse=True)
+    elif sort_by == 'author':
+        citations.sort(key=lambda x: x.author.lower())
+    elif sort_by == 'title':
+        citations.sort(key=lambda x: x.title.lower())
+
+    return citations
 
 def create_article(info:Article):
     sql = text('''INSERT INTO articles (key, author, title, journal, year, volume, pages)
