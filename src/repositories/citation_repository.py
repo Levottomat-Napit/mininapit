4ae483d2d1c403a931af694000c472354c06680d
Hánno Länsman <halansman@gmail.com>
2024-12-09 15:08:49 +0200

    Unified Isorisus and my codes to include book-citation

--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -2,16 +2,26 @@ from sqlalchemy import text
 from config import db
 from entities.citation import Article, Inproceedings, Book
 
-def get_citations():
+def get_citations(sort_by:None):
     articles = db.session.execute(text('SELECT * FROM articles')).mappings().fetchall()
     inproceedings = db.session.execute(text('SELECT * FROM inproceedings')).mappings().fetchall()
     books = db.session.execute(text('SELECT * FROM books')).mappings().fetchall()
 
-    return (
+    citations = [
         [Article(**article) for article in articles] +
         [Inproceedings(**inproceeding) for inproceeding in inproceedings] +
         [Book(**book) for book in books]
-    )
+    ]
+    if sort_by is None:
+        pass
+    elif sort_by == 'year':
+        citations.sort(key=lambda x: x.year, reverse=True)
+    elif sort_by == 'author':
+        citations.sort(key=lambda x: x.author.lower())
+    elif sort_by == 'title':
+        citations.sort(key=lambda x: x.title.lower())
+
+    return citations
 
 def create_article(info: Article):
     sql = text('''INSERT INTO articles (key, author, title, journal, year, volume, pages)
