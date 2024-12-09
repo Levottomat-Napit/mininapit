ce90b34ef3d21b9e76542199d4e350888921f2d5
Merentie <mikael.merentie@gmail.com>
2024-12-09 18:44:23 +0200

    Update doi_search

--- src/doi_search.py
+++ src/doi_search.py
@@ -1,31 +1,44 @@
 import urllib.request, json
+from entities.citation import Article, Inproceedings, Book
+from repositories.citation_repository import create_article, create_inproceedings, create_book
 
 #Early proof of concept-version of the doi search function
 
-def doi_search(doi):
-    with urllib.request.urlopen(f"https://api.crossref.org/works/{doi}") as url:
-        data = json.load(url)
-        print(data)
-        data_type = data["message"]["type"]
-        data_author = []
-        data_year = (data["message"]["indexed"]["date-time"])
-        authors = data["message"]["author"]
-        for author in authors:
-            data_author.append(author["family"]+" "+author["given"])
-        data_author = ", ".join(data_author)
-        print(data_author)
-        print(data_year[:4])
+def doi_search(doi, data_key):
+    try:
+        with urllib.request.urlopen(f"https://api.crossref.org/works/{doi}") as url:
+            data = json.load(url)
+    except:
+            print("URL not found")
+            return
+    print(data)
+    data_type = data["message"]["type"]
+    data_title = data["message"]["title"][0]
+    data_author = []
+    data_year = ((data["message"]["indexed"]["date-time"])[:4])
+    authors = data["message"]["author"]
+    for author in authors:
+        data_author.append(author["given"]+" "+author["family"])
+    data_author = ", ".join(data_author)
+    print(data_author)
+    print(data_year)
 
-        # if data_type == "journal-article":
-        #     pass
-        # elif data_type == "proceedings-article":
-        #     pass
-        # elif data_type == "book":
-        #     pass
-        # else:
-        #     pass
-        # title = data["message"]["title"][0]
+    if data_type == "journal-article":
+        data_journal = data["message"]["container-title"][0]
+        article = Article(
+            key=data_key, 
+            author=data_author, 
+            title=data_title, 
+            journal = data_journal, 
+            year = data_year)
+        create_article(article)
+    elif data_type == "proceedings-article":
+        pass
+    elif data_type == "book":
+        pass
+    else:
+        pass
         
 
 # An example search
-doi_search("10.1145/3551349.3559494")
+doi_search("10.10.1109/TASLP.2019.2950099", "rust")
